1c8908e5418927c0a3cf71ddea6d24eedaa6f33c
eceeunal <ecedogann3@gmail.com>
2024-05-01 15:51:33 +0300

    clarus-store

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import Login from './pages/Login';
-import './App.css';
+// import Home from "./pages/Home";
+// import Login from "./pages/Login";
+// import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import Products from "./pages/Products";
+// import About from "./pages/About";
+// import NotFound from "./pages/NotFound";
+
+import AuthProvider from "./context/AuthProvider";
+import ProductProvider from "./context/ProductProvider";
+import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
     <div>
-      <Login />
+      <AuthProvider>
+        <ProductProvider>
+          <AppRouter />
+        </ProductProvider>
+      </AuthProvider>
+      {/*//* route yapısını router dosyasına taşıdık */}
       
     </div>
+  
   );
 }
 
 export default App;
+
